b329ba9d30a0e9faa344fbf9c25d53bf2fb6777e
dbasri <daryl_basri@sfu.ca>
2020-06-24 18:03:44 +0700

    add help activity

---
+++ app/src/main/java/cmpt276/as1/mineseeker/HelpActivtiy.java
@@ -0,0 +1,14 @@
+package cmpt276.as1.mineseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpActivtiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_activtiy);
+    }
+}
